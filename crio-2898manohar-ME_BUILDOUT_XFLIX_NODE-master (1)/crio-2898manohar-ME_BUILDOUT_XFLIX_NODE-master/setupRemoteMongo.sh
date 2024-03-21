03cd5f55a255bafd3245d7bb5ceeb362aeed590c
manohar12-max <127774463+manohar12-max@users.noreply.github.com>
2024-03-21 17:20:50 +0530

    Add files via upload

---
+++ crio-2898manohar-ME_BUILDOUT_XFLIX_NODE-master (1)/crio-2898manohar-ME_BUILDOUT_XFLIX_NODE-master/setupRemoteMongo.sh
@@ -0,0 +1,3 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb+srv://xflix:xflixpass@mongo_server_url/<dbname>?retryWrites=true&w=majority" 
+mongoimport --uri "mongodb+srv://xflix:xflixpass@mongo_server_url/<dbname>?retryWrites=true&w=majority" 
